67d8988c8086a64f4032af054d94c612584d738a
Ogi62 <danguba962@gmail.com>
2023-06-27 21:32:09 +0200

    add routes, make table for users make post for signup

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 let cors = require("cors");
-
+const connection = require("./connection");
+const userRoute = require("./routes/user");
 const app = express();
+
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use("/user", userRoute);
 
 module.exports = app;
