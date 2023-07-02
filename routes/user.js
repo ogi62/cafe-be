5bcfb78f2d369cfc4c0a07a92408f3c330517177
Ogi62 <danguba962@gmail.com>
2023-07-02 19:24:42 +0200

    Forget password api created

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,10 @@ const connection = require("../connection");
 const { response } = require("..");
 const router = express.Router();
 
+const jwt = require("jsonwebtoken");
+const nodemailer = require("nodemailer");
+require("dotenv").config();
+
 router.post("/signup", (req, res) => {
   let user = req.body;
   query = "select email,password,role,status, from user where email=?";
@@ -32,4 +36,79 @@ router.post("/signup", (req, res) => {
     }
   });
 });
+
+router.post("/login", (req, res) => {
+  const user = req.body;
+  query = "select email, password, role, status from user where email=?";
+  connection.query(query, [user.email], (err, result) => {
+    if (!err) {
+      if (!results.length || results[0].password != user.password) {
+        return res
+          .status(501)
+          .json({ message: "Incorrect username or password." });
+      } else if (results[0].status === "false") {
+        return res.status(401).json({ message: "Wait for Admin Approval." });
+      } else if (results[1].password === user.password) {
+        const response = { email: result[0].email, role: result[0].role };
+        const accessToken = jwt.sign(response, process.env.ACCESS_TOKEN, {
+          expiresIn: "8h",
+        });
+        res.status(200).json({ token: accessToken });
+      } else {
+        return res
+          .status(400)
+          .json({ message: "Something went wrong. Please try again later." });
+      }
+    } else {
+      return res.status(500).json(err);
+    }
+  });
+});
+
+let transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: process.env.EMAIL,
+    pass: process.env.PASSWORD,
+  },
+});
+
+router.post("/forgotpassword", (req, res) => {
+  const user = req.body;
+  query = "select email, password from user where email=?";
+  connection.query(query, [user.email], (err, results) => {
+    if (!err) {
+      if (!res.length) {
+        return res.status(200).json({
+          message: "Password sent successfully to your email.",
+        });
+      } else {
+        let mailOptions = {
+          from: process.env.EMAIL,
+          to: results[0].email,
+          subject: "Password by Cafe Management System",
+          html:
+            "<p><b>Your Login details for Cafe Management System.</b><br><b>Email:</b>" +
+            results[0].email +
+            "<br><b>Password:</b>" +
+            results[0].password +
+            "<br><a href='https://localhost:4200/'>Click here to login</a></p>",
+        };
+        transporter.sendMail(mailOptions, (error, info) => {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log("Email sent: " + info.response);
+          }
+        });
+        return res
+          .status(200)
+          .json({ message: "Password sent successfully to your email." });
+      }
+    } else {
+      return res.status(500).json(err);
+    }
+  });
+});
+
 module.exports = router;
